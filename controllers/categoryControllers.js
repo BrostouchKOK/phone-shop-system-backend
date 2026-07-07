79718cc5979445ed4e88b210ca91b8b94360fc32
Brostouch KOK <brostouchkokit1007@gmail.com>
2026-07-07 20:16:39 +0700

    create backend project, config database, cloudinary, category and product models

---
+++ controllers/categoryControllers.js
@@ -0,0 +1,4 @@
+import Category from "../models/Category.js";
+
+// @desc    បង្កើត Category ថ្មី (Admin Only)
+// @route   POST /api/categories
---
+++ models/Product.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    brand: { type: String, required: true },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    price: { type: Number, required: true, default: 0 },
+    description: { type: String, required: true },
+    stock: { type: Number, required: true, default: 0 },
+    image: [{ type: String, required: true }],
+  },
+  { timestamps: true },
+);
+
+const Product = mongoose.model('Product',productSchema);
+export default Product;
